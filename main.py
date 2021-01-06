fb6099672e9e707653effb0ce4a94ccf200385b5
Ananya Dharna <claryh60@gmail.com>
2021-01-06 04:20:40 +0000

    I added the list of words and warnings(Anti-Swear)

--- main.py
+++ main.py
@@ -40,5 +40,12 @@ async def on_message(message):
         await message.channel.send ('```** Hi there ! Its me Hi Bot. M Designed by a Stupid novice coder named Annie (Ananya xD) who hasn\'t even passed her 10th ..Howtf :/ \n Kek All I can do is nothing but this Auto Response T_T \n Type \n #help \n \n For Fun) **```')
 
 
-client.run(os.getenv('TOKEN'))
+swear_words = ["fuck", "motherfucker", "nigga", "bitch", "cock"]
 
+starter_encouragements = [
+"u mf u don't swear in this server",
+"swearing isnt allowed in dankers op",
+"you will get warned if u swear again"
+]
+
+client.run(os.getenv('TOKEN'))
