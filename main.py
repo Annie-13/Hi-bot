891c86e9c64aad6f6309f3d66c3e3a213e82e3d0
Ananya Dharna <claryh60@gmail.com>
2021-01-06 04:47:07 +0000

    I made an auto response for swear words along with options to change(but previous code doesnt work)

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import requests
 import json
 import random
+from replit import db
 
 class color:
    PURPLE = '\033[95m'
@@ -42,10 +43,42 @@ async def on_message(message):
 
 swear_words = ["fuck", "motherfucker", "nigga", "bitch", "cock"]
 
-starter_encouragements = [
+warns = [
 "u mf u don't swear in this server",
 "swearing isnt allowed in dankers op",
 "you will get warned if u swear again"
 ]
+
+if "responding" not in db.keys():
+  db["responding"] = True
+
+def warn_update(warns):
+  if "warns" in db.keys():
+    warns = db["warns"]
+    warns.append(warn)
+    db["warns"] = warns
+  else:
+    db["warns"] = [warns]
+
+def delete_encouragment(index):
+  warns = db["warns"]
+  if len(warns) > index:
+    del warns[index]
+  db["warns"] = warns
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  msg = message.content
+
+  if db["responding"]:
+    options = warns
+
+    if any(word in msg for word in swear_words):
+      await message.channel.send(random.choice(options))
+
+
 
 client.run(os.getenv('TOKEN'))
